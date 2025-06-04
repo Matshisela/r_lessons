fcc477c64d86de25eef5b4abd3ea0789adb4de75
Matshisela <drmatshisela@gmail.com>
2025-06-04 14:29:36 +0200

    work

---
+++ sect_2_data_wrangling_assignment.R
@@ -0,0 +1,19 @@
+# Author: Ntando
+# Date: 2025-06-04
+# Description: Analyzng heart disease dataset using R.
+# Github Link: https://github.com/your_repo
+
+
+# Load necessary libraries
+library(tidyverse)
+
+
+# get data from the URL
+url <- "https://raw.githubusercontent.com/sharmaroshan/Heart-UCI-Dataset/refs/heads/master/heart.csv"
+
+# Read the dataset
+heart_data <- read_csv(url)
+
+
+# Perform data cleaning
+
